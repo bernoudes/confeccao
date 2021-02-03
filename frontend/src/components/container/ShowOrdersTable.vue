ee808452fce29f3214c07b0bee76709c95caad46
Bernoudes <lucasbernoudes@gmail.com>
2021-02-03 07:06:23 -0300

    modificaçõs no visual da tabela e adicionamento da tela de edição

--- frontend/src/components/container/ShowOrdersTable.vue
+++ frontend/src/components/container/ShowOrdersTable.vue
@@ -1,54 +1,51 @@
 <template>
     <div class="showOrdersTable">
         <b-table id="idTable" sticky-header bordered class="table text-center text-nowrap"
-            striped :fields="orders.fields" :items="orders.items" small>
-
+            striped :fields="orders.fields" :items="orders.items" @dblclick="usable" small>
+            <template #cell(bu_editable)="row">
+                <i class="fa fa-edit" @click='editConfg(row)'></i>
+            </template>
         </b-table>
+
+        <EditOrders class="editOrders"  name="ShowOrders_editorOrders"
+            :item="selectedItem"/>
     </div>
 </template>
 
 <script>
 
 import ShowOrdersTableConfig from './ShowOrderTableConfig.js'
+import EditOrders from './EditOrders'
 
 export default {
     name: 'showOrdersTable',
     data(){
         return{
             orders:{
-                items: ShowOrdersTableConfig.items,
+                selectedItem: null,
+                items: ShowOrdersTableConfig.items(this.filter),
                 fields: ShowOrdersTableConfig.Fields
-            }
+            },
+            editorHtml:'hfdh'
         }
     },
+    components:{ EditOrders },
+    props:{
+        filter:{}
+    },
     methods:{
-        testing(test){
-            console.log(test)
+        usable(){
+            console.log('kasa hitotsu')
+        },
+        editConfg(row){
+            this.selectedItem = row.item;
+            const editOrdersElement = document.getElementsByName('ShowOrders_editorOrders')[0]
+            editOrdersElement.style.display = "initial"
         }
     },
     watch:{
-        'idTable'() {
-            this.orders.items.forEach(element => {
-                if(element.status_order == 'Prosseguindo'){
-                    element._cellVariants= { status_order: 'success'}
-                } 
-                if(element.status_order == 'Alerta'){
-                    element._cellVariants= {status_order: 'warning '}
-                } 
-                if(element.status_order == 'Atrasado'){
-                    element._rowVariant='danger'
-                } 
-                if(element.status_order == 'Finalizado'){
-                    element._rowVariant='primary'
-                } 
-                if(element.status_order == 'Entregue'){
-                    element._rowVariant='success '
-                } 
-                if(element.status_order == 'Cancelado'){
-                    element._rowVariant='dark'
-                }
-            })
-            console.log('hum')
+        filter: function (filter) {
+            this.items = ShowOrdersTableConfig.items(filter)
         }
     }
 }
@@ -60,6 +57,12 @@ export default {
     }
     .showOrdersTable .table{
         max-height: 100%;
+    }
+    .showOrdersTable .editOrders{
+        position:absolute;
+        margin-top: -600px;
+        margin-left:150px;
+        z-index: 4;
     }
 
 </style>
